aca019329357bb15eb2072d6ec508125348ac0e7
Chanchal Jaiswal <chanchal123jaiswal@gmail.com>
2026-06-03 15:13:08 +0530

    backend

---
+++ server.js
@@ -0,0 +1,14 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+    res.send("Hello from the backend");
+});
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
