90aa768b913c92cfc8bcca77912cb342b959c163
Chanchal Jaiswal <chanchal123jaiswal@gmail.com>
2026-06-05 10:16:44 +0530

    deploy detup

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const app = express();
 
 // ✅ FIX 1: CORS (production safe)
 app.use(cors({
-  origin:  "http://localhost:5173",
+  origin:  process.env.CLIENT_URL,
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"]
 }));
