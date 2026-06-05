9b86333dc342012be62b618a33620eae4102c8cd
Chanchal Jaiswal <chanchal123jaiswal@gmail.com>
2026-06-05 08:39:41 +0530

    bug fix

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 import express from "express";
 import connectDB from "./config/db.js";
 import dotenv from "dotenv";
+dotenv.config();
+
 import authRoute from "./routes/authRoute.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import adminRoute from "./routes/adminRoute.js";
-dotenv.config();
 connectDB();
 const app = express();
-app.use(cors({ credentials: true, origin: true }));
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 const PORT = process.env.PORT || 5000;
@@ -18,6 +19,6 @@ app.get("/", (req, res) => {
   res.send("Hello from the backend");
 });
 
-app.listen(process.env.PORT, () => {
+app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
