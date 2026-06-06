320f0e941d4e5e1262db33a547c8b02178cf34bc
Chanchal Jaiswal <chanchal123jaiswal@gmail.com>
2026-06-06 07:13:28 +0530

    logout

--- controllers/authController.js
+++ controllers/authController.js
@@ -62,7 +62,6 @@ export const login = async (req, res) => {
 // logout
 export const logout = async (req, res) => {
   try {
-    res.clearCookie("token");
     res.status(200).json({ message: "Logout successful" });
   } catch (error) {
     res.status(500).json({ error: error.message });
